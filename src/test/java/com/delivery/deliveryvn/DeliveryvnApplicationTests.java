770d0faa54712ed5e75124415838c8e0fdf259e2
hoanganh23 <hoanganhnth2k3@gmail.com>
2025-06-22 13:07:43 +0700

    init

---
+++ src/test/java/com/delivery/deliveryvn/DeliveryvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.deliveryvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
